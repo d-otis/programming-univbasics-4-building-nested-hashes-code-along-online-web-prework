221b3a2c95b2bc53b544cb79b2e97affe6c350c9
Dan Foley <dan@dan-foley.com>
2019-12-12 20:05:30 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -30,9 +30,9 @@ def fourth_challenge
           :attitude => "confused"
         },
         {
-          :name => "",
-          :age => "",
-          :attitude =>
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
         }
       ]
    }
